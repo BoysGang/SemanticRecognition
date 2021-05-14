1f0533d2caa203f400c5358f6f56af9a7003b882
Max Borzenko <zzzking999@yandex.ru>
2021-05-14 15:18:43 +0700

    trying cnn sigmoid

--- src/predicting.py
+++ src/predicting.py
@@ -1,9 +1,7 @@
+import os
 import joblib
 import numpy as np
-
-from skimage.transform import resize
-from skimage.io import imread
-from sklearn.linear_model import LogisticRegression
+import pickle
 
 from tensorflow.keras import models
 from tensorflow.keras.preprocessing import image
@@ -21,12 +19,15 @@ def print_probabilities(labels, probabilities):
 
 def predict(model_path, img_path, width, height):
     model = models.load_model(model_path)
+    
+    pickle_in = open(os.path.join(model_path, "labels"), "rb")
+    labels =  pickle.load(pickle_in)
 
-    img = image.load_img(img_path, target_size=(width, height))
+    img = image.load_img(img_path, target_size=(width, height), color_mode="grayscale")
     x = image.img_to_array(img) / 255
     x = np.expand_dims(x, axis=0)
 
     images = np.vstack([x])
     probabilities = model.predict(images, batch_size=10)
 
-    print(probabilities.tolist()[0])
+    print_probabilities(labels, probabilities)
--- src/training.py
+++ src/training.py
@@ -1,16 +1,10 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-import joblib
-
+import pickle
 
 from sklearn import metrics
-from skimage.io import imread
-from skimage.transform import resize
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-
 
 from tensorflow.keras import datasets, layers, models
 from keras_preprocessing.image import ImageDataGenerator
@@ -24,6 +18,7 @@ def train(data_path, model_path, width=80, height=None):
     train_generator = training_datagen.flow_from_directory(
         TRAINING_DIR,
         target_size=(width, height),
+        color_mode="grayscale",
         class_mode='categorical',
         batch_size=126
     )
@@ -41,21 +36,27 @@ def train(data_path, model_path, width=80, height=None):
         
         layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
         layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.5),
         
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
-        layers.Dense(2, activation='softmax')
+        layers.Dropout(0.5),
+        layers.Dense(4, activation='sigmoid')
     ])
 
     cnn.summary()
 
     cnn.compile(optimizer='adam',
-              loss='categorical_crossentropy',
+              loss='binary_crossentropy',
               metrics=['accuracy'])
     
-    cnn.fit(train_generator, epochs=5)
+    cnn.fit(train_generator, epochs=30)
 
-    # Cannot add just list of labels
-    cnn.labels = np.array(labels)
-    
     cnn.save(model_path)
+
+    pickle_out = open(os.path.join(model_path, "labels"), "wb")
+    pickle.dump(labels, pickle_out)
+    pickle_out.close()
